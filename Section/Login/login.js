2e90710e1fd3b5835258e428f8ebe8c6d2c488dc
Sushant Bhosale <bhosalesushant0@gmail.com>
2024-10-16 00:43:54 +0530

    Project Pushed

---
+++ Section/Login/login.js
@@ -0,0 +1,42 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAT69wmPHm9tZ3xBWhpvAVO5hRJsQ-WSC0",
+  authDomain: "smarteats-bc8c9.firebaseapp.com",
+  projectId: "smarteats-bc8c9",
+  storageBucket: "smarteats-bc8c9.appspot.com",
+  messagingSenderId: "915448268413",
+  appId: "1:915448268413:web:569c30db72dbd90b739314",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const submit = document.getElementById("login-submit");
+submit.addEventListener("click", function (event) {
+  event.preventDefault();
+  const email = document.getElementById("login-email").value;
+  const password = document.getElementById("login-password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed up
+      const user = userCredential.user;
+      window.location.href='/Section/PersonalisedDiet/PersonalisedDiet.html'
+      // You can also redirect the user to another page or perform other actions here
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(`Error: ${errorMessage}`);
+      // You can also handle specific error codes here
+    });
+});
+
+
+
+
